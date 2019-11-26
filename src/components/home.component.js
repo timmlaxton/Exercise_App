7a1635595260a4b7dddf82888262182475ee52aa
System Administrator <root@tim.home>
2019-11-26 11:28:26 +0000

    added tabs to home

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import {Tabs, Tab} from 'react-mdl';
 
-export default class Home extends Component {
-  render () {
-    return (
-      <div>
-      <p>sdfghgfdsdfgf</p>
-      </div>
-    )
+
+ class Home extends Component {
+   constructor(props){
+     super(props);
+     this.state = {activeTab: 0};
    }
+
+
+   render() {
+     return(
+       <div className="category-tabs">
+       <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+       <Tab>Running</Tab>
+       <Tab>Swimming</Tab>
+       <Tab>Yoga</Tab>
+       <Tab>Cycling</Tab>
+       </Tabs>
+       </div>
+
+   )
   }
+ }
+
+export default Home
