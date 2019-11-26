45eb864735e5f3ff1ceba580808a6da47e2090ae
System Administrator <root@tim.home>
2019-11-26 12:35:07 +0000

    added images

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,27 +1,136 @@
 import React, { Component } from 'react';
-import {Tabs, Tab} from 'react-mdl';
-
+import { Tabs, Tab, Grid, Row, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
  class Home extends Component {
    constructor(props){
-     super(props);
+     super(props)
      this.state = {activeTab: 0};
    }
+   toggleCategories() {
+
+      if(this.state.activeTab === 0){
+        return(
+
+        <div className="card-grid">
 
 
-   render() {
-     return(
-       <div className="category-tabs">
-       <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
-       <Tab>Running</Tab>
-       <Tab>Swimming</Tab>
-       <Tab>Yoga</Tab>
-       <Tab>Cycling</Tab>
-       </Tabs>
-       </div>
+            {/* Project 1 */}
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/running-is-one-of-the-best-ways-to-stay-fit-royalty-free-image-1568757836.jpg?crop=0.668xw:1.00xh;0.287xw,0&resize=480:*) center / cover'}} ></CardTitle>
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              </CardText>
+              <CardActions border>
+                <Button colored>GitHub</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
 
-   )
+
+
+            {/* Project 2 */}
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://3v718laqyo244ii5v20dg6ff-wpengine.netdna-ssl.com/wp-content/uploads/2017/10/5-Fixes-for-Cycling-Related-Lower-Back-Pain-752x472.jpg) center / cover'}} ></CardTitle>
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              </CardText>
+              <CardActions border>
+                <Button colored>GitHub</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+            {/* Project 3 */}
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.outsideonline.com/sites/default/files/styles/img_600x600/public/2019/05/29/open-water-swimmer_s.jpg?itok=9KoLuX6-) center / cover'}} ></CardTitle>
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              </CardText>
+              <CardActions border>
+                <Button colored>GitHub</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+            {/* Project 4 */}
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://c.shetland.org/assets/files/14786/climbing_eshaness_mike_hutton.1680x0.jpeg) center / cover'}} ></CardTitle>
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              </CardText>
+              <CardActions border>
+                <Button colored>GitHub</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://image.shutterstock.com/image-photo/row-kettlebell-girya-weights-gym-260nw-622238240.jpg) center / cover'}} ></CardTitle>
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              </CardText>
+              <CardActions border>
+                <Button colored>GitHub</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.outsideonline.com/sites/default/files/styles/img_600x600/public/2019/05/29/open-water-swimmer_s.jpg?itok=9KoLuX6-) center / cover'}} ></CardTitle>
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              </CardText>
+              <CardActions border>
+                <Button colored>GitHub</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+
+
+            </div>
+
+
+        )
+      }
+    }
+    render() {
+      return(
+        <div>
+
+            <Grid>
+              <Cell col={12}>
+                <div className="content">{this.toggleCategories()}</div>
+              </Cell>
+            </Grid>
+        </div>
+      )
+    }
   }
- }
+
 
 export default Home
