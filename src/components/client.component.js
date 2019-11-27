5bb0973fa551006f3e8bf5db3977d9e4c73c4e96
System Administrator <root@tim.home>
2019-11-27 12:24:28 +0000

    images clients

--- src/components/client.component.js
+++ src/components/client.component.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
+ export default class Home extends Component {
+   render() {
+     return(
 
-
- class Client extends Component {
-   constructor(props){
-     super(props)
-     this.state = {activeTab: 0};
-   }
-   toggleCategories() {
-
-      if(this.state.activeTab === 0){
-        return(
-
-        <div className="card-grid">
-
-
-            {/* Project 1 */}
+       <div style={{width: '80%', margin: 'auto'}}>
+        <Grid className="New-user"/>
+         <Cell col={3} />
             <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/running-is-one-of-the-best-ways-to-stay-fit-royalty-free-image-1568757836.jpg?crop=0.668xw:1.00xh;0.287xw,0&resize=480:*) center / cover'}} ></CardTitle>
+              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://images.unsplash.com/photo-1549190179-646f048c6108?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60) center / cover'}} ></CardTitle>
               <CardText>
               Create New User
               </CardText>
@@ -26,37 +17,6 @@ import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, I
               <Button href="/user" colored>New User</Button>
 
 
-              </CardActions>
-              <CardMenu style={{color: '#fff'}}>
-                <IconButton name="share" />
-              </CardMenu>
-            </Card>
-
-
-
-            {/* Project 2 */}
-            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://3v718laqyo244ii5v20dg6ff-wpengine.netdna-ssl.com/wp-content/uploads/2017/10/5-Fixes-for-Cycling-Related-Lower-Back-Pain-752x472.jpg) center / cover'}} ></CardTitle>
-              <CardText>
-              Create New Routine
-              </CardText>
-              <CardActions border>
-                <Button href="/create" colored>Choose A Routine</Button>
-
-              </CardActions>
-              <CardMenu style={{color: '#fff'}}>
-                <IconButton name="share" />
-              </CardMenu>
-            </Card>
-
-            {/* Project 3 */}
-            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-              <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.outsideonline.com/sites/default/files/styles/img_600x600/public/2019/05/29/open-water-swimmer_s.jpg?itok=9KoLuX6-) center / cover'}} ></CardTitle>
-              <CardText>
-                Exercise Log
-              </CardText>
-              <CardActions border>
-                <Button href="/exercise" colored>Client Log</Button>
 
               </CardActions>
               <CardMenu style={{color: '#fff'}}>
@@ -65,29 +25,51 @@ import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, I
             </Card>
 
 
+            <div style={{width: '80%', margin: 'auto'}}>
+             <Grid className="New-log"/>
+              <Cell col={3} />
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://images.unsplash.com/photo-1549190179-646f048c6108?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60) center / cover'}} ></CardTitle>
+            <CardText>
+            Create New log
+            </CardText>
+            <CardActions border>
+            <Button href="/user" colored>New log</Button>
 
 
-            </div>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <div style={{width: '80%', margin: 'auto'}}>
+           <Grid className="Custom-regime"/>
+           <Cell col={3} />
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://images.unsplash.com/photo-1549190179-646f048c6108?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60) center / cover'}} ></CardTitle>
+          <CardText>
+          Create New regime
+          </CardText>
+          <CardActions border>
+          <Button href="/user" colored>New regime</Button>
 
 
-        )
-      }
-    }
-    render() {
-      return(
-
-        <div className="profile-grid">
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
 
 
-            <Grid>
-              <Cell col={12} row={6}>
-                <div className="content">{this.toggleCategories()}</div>
-              </Cell>
-            </Grid>
-        </div>
+
+              </div>
+              </div>
+              </div>
+
+
+
+
       )
     }
   }
-
-
-export default Client
--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,63 +1,10 @@
 import React, { Component } from 'react';
 import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
-
+import background from "../images/background.jpg";
 
 
    class Home extends Component {
-     constructor(props){
-       super(props)
-       this.state = {activeTab: 0};
-     }
-     toggleCategories() {
-
-        if(this.state.activeTab === 0){
-          return(
-
-          <div className="card-grid">
-
-
-              {/* Project 1 */}
-              <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/running-is-one-of-the-best-ways-to-stay-fit-royalty-free-image-1568757836.jpg?crop=0.668xw:1.00xh;0.287xw,0&resize=480:*) center / cover'}} ></CardTitle>
-                <CardText>
-                Create New User
-                </CardText>
-                <CardActions border>
-                <Button href="/user" colored>New User</Button>
-
-
-                </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                  <IconButton name="share" />
-                </CardMenu>
-              </Card>
-
-
-
-              {/* Project 2 */}
-              <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.success.edu/wp-content/uploads/2014/04/north-west-college-health-care-careers-professional-fitness-trainer.jpg) center / cover'}} ></CardTitle>
-                <CardText>
-                Trainer
-                </CardText>
-                <CardActions border>
-                  <Button href="/create" colored>qwqw</Button>
-
-                </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                  <IconButton name="share" />
-                </CardMenu>
-              </Card>
-
-
-</div>
-
-
-)
-}
-}
-
-  render() {
+    render() {
   return(
 
 
@@ -65,16 +12,11 @@ import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, I
         <h1> Welcome to Excer Tracker </h1>
 
 
-        <div className="profile-grid">
-
-
-            <Grid>
-              <Cell col={12} row={6}>
-                <div className="content">{this.toggleCategories()}</div>
-              </Cell>
-            </Grid>
+        <div className="back-img">
+        <img src={background}/>
         </div>
         </div>
+
       )
     }
   }
